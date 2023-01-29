d7fe353642fe15778498771c64629a36629c21fc
Andrew <93141508+svpra@users.noreply.github.com>
2023-01-29 21:55:00 +0300

    add key to model

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,8 +2,11 @@ using System;
 
 namespace Tectex.Models {
     public class UserModel {
+        [Key]
+        public int Id { get; set; }
         public string Nick { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
