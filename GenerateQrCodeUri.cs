c1a23bb7e3937dbdc398a0735578b5ef5c89ed99
Andrew <93141508+svpra@users.noreply.github.com>
2023-01-29 23:30:54 +0300

    Add namespace

--- GenerateQrCodeUri.cs
+++ GenerateQrCodeUri.cs
@@ -1,8 +1,21 @@
-private string GenerateQrCodeUri(string email, string unformattedKey)
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Manage.Internal
+
+namespace Tectex 
 {
-    return string.Format(
-        AuthenticatorUriFormat,
-        _urlEncoder.Encode("AppForAhahAndHehe"),
-        _urlEncoder.Encode(email),
-        unformattedKey);
+    private string GenerateQrCodeUri(string email, string unformattedKey)
+    {
+        return string.Format(
+            AuthenticatorUriFormat,
+            _urlEncoder.Encode("AppForAhahAndHehe"),
+            _urlEncoder.Encode(email),
+            unformattedKey);
+    }
 }
