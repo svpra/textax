43aadc79119c8487ca6fb751fa493f9b6661f834
Andrew <93141508+svpra@users.noreply.github.com>
2023-01-29 23:27:43 +0300

    generate qr for totp

---
+++ GenerateQrCodeUri.cs
@@ -0,0 +1,8 @@
+private string GenerateQrCodeUri(string email, string unformattedKey)
+{
+    return string.Format(
+        AuthenticatorUriFormat,
+        _urlEncoder.Encode("Razor Pages"),
+        _urlEncoder.Encode(email),
+        unformattedKey);
+}
