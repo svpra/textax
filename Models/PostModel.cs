58a101488ef5ec9d776156fecd2ad40331da3dce
Andrew <93141508+svpra@users.noreply.github.com>
2023-01-29 12:52:48 +0300

    Add Title pole to model

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Tectex.Models {
     public class Post {
+        public string Title { get; set; }
         public string Content { get; set; }
         public string AuthorId { get; set; }
         public User Author { get; set; }
