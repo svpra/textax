5ac312ff576716806107592d1c891aaac1349497
Andrew <93141508+svpra@users.noreply.github.com>
2023-01-30 23:07:49 +0300

    Update model for gh oauth

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -8,5 +8,18 @@ namespace Tectex.Models {
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Url { get; set; }
+        public Avatar { get; set; }
+        
+        public void OnGet()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                Name = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                Nick = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+                Url = User.FindFirst(c => c.Type == "urn:github:url")?.Value;
+                Avatar = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+            }
+        }
     }
 }
