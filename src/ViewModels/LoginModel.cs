7e9199b464eba90c32782c5da70870a808308948
Andrew <93141508+svpra@users.noreply.github.com>
2023-02-02 22:16:22 +0300

    Add RegisterViewModel to ViewModel

--- src/ViewModels/LoginModel.cs
+++ src/ViewModels/LoginModel.cs
@@ -2,20 +2,37 @@ using System.ComponentModel.DataAnnotations;
  
 namespace Texta.ViewModels
 {
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+ 
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+ 
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
     public class LoginViewModel
     {
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
-         
+ 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
         public string Password { get; set; }
-         
+ 
         [Display(Name = "Запомнить?")]
         public bool RememberMe { get; set; }
-         
+ 
         public string ReturnUrl { get; set; }
     }
 }
