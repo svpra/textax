f21c7bf0df2f363bb089d0d7e72af10e88943ab8
Andrew <93141508+svpra@users.noreply.github.com>
2023-02-10 22:39:57 +0300

    Add likes and text to main model

--- src/Texta.Domain/Entities/Post/Post.cs
+++ src/Texta.Domain/Entities/Post/Post.cs
@@ -3,7 +3,9 @@ namespace Domain.Entities.Post;
 public class Post
 {
     public long Id { get; set; }
+    public int Likes { get; set; }
     public string Title { get; set; } = null!;
     public string ShortDescription { get; set; } = null!;
     public string Link { get; set; } = null!;
+    public string Text { get; set; } = null!;
 }
